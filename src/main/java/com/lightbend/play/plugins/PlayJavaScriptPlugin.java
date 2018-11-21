fa57be4c87e50c96a03a99d58d2a464f7efd7502
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-11-21 10:03:21 -0700

    For now strip away anything beyond the plugin-level

--- src/main/java/com/lightbend/play/plugins/PlayJavaScriptPlugin.java
+++ src/main/java/com/lightbend/play/plugins/PlayJavaScriptPlugin.java
@@ -1,11 +1,11 @@
 package com.lightbend.play.plugins;
 
-import com.lightbend.play.tasks.JavaScriptMinify;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.plugins.BasePlugin;
 import org.gradle.play.platform.PlayPlatform;
+import org.gradle.play.tasks.JavaScriptMinify;
 
 import java.io.File;
 
--- src/main/java/com/lightbend/play/tasks/StaleClassCleaner.java
+++
@@ -1,9 +0,0 @@
-package com.lightbend.play.tasks;
-
-import java.io.File;
-
-public abstract class StaleClassCleaner {
-    public abstract void execute();
-
-    public abstract void addDirToClean(File toClean);
-}
