7e37435de04caaef6a5f9b6d22d3f3bf46bd6b0b
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-11-28 11:38:44 -0700

    Add integration test for multi-project build

---
+++ src/integTest/groovy/com/lightbend/play/application/PlayMultiProjectApplicationIntegrationTest.groovy
@@ -0,0 +1,37 @@
+package com.lightbend.play.application
+
+import com.lightbend.play.AbstractIntegrationTest
+import com.lightbend.play.fixtures.app.PlayApp
+import com.lightbend.play.fixtures.app.PlayMultiProject
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+
+class PlayMultiProjectApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    PlayApp playApp = new PlayMultiProject()
+
+    def setup() {
+        playApp.writeSources(projectDir)
+    }
+
+    def "can build play app binary"() {
+        when:
+        BuildResult result = build(":primary:assemble")
+
+        then:
+        result.task(":javalibrary:jar").outcome == TaskOutcome.SUCCESS
+        result.task(":primary:assemble").outcome == TaskOutcome.SUCCESS
+
+        and:
+        jar("primary/build/libs/primary.jar").containsDescendants(
+                "router/Routes.class",
+                "controllers/Application.class")
+        jar("primary/build/libs/primary-assets.jar").hasDescendants(
+                "public/primary.txt")
+        // TODO: JARs need to be declared as outgoing artifacts
+//        jar("submodule/build/libs/submodule.jar").containsDescendants(
+//                "controllers/submodule/Application.class")
+//        jar("submodule/build/libs/submodule-assets.jar").hasDescendants(
+//                "public/submodule.txt")
+    }
+}
---
+++ src/integTestFixtures/groovy/com/lightbend/play/fixtures/app/PlayMultiProject.groovy
@@ -0,0 +1,19 @@
+package com.lightbend.play.fixtures.app
+
+import org.gradle.util.VersionNumber
+
+class PlayMultiProject extends PlayApp {
+
+    PlayMultiProject() {
+        super()
+    }
+
+    PlayMultiProject(VersionNumber version) {
+        super(version)
+    }
+
+    @Override
+    List<SourceFile> getAllFiles() {
+        return sourceFiles("primary") + sourceFiles("submodule") + sourceFiles("javalibrary") + sourceFile("", "settings.gradle")
+    }
+}
