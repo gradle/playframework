e05a584633a3fbbe9327bf83b73849a4adcb39c8
Sterling Greene <sterling.greene@gmail.com>
2019-09-17 00:58:27 -0400

    Use convention over set in a few places

--- src/main/java/org/gradle/playframework/extensions/PlayExtension.java
+++ src/main/java/org/gradle/playframework/extensions/PlayExtension.java
@@ -31,12 +31,11 @@ public class PlayExtension {
     private final Property<Boolean> injectedRoutesGenerator;
 
     public PlayExtension(ObjectFactory objectFactory) {
-        platform = objectFactory.newInstance(PlayPlatform.class, objectFactory);
-        platform.getPlayVersion().set(DEFAULT_PLAY_VERSION);
-        platform.getScalaVersion().set(platform.getPlayVersion().map(playVersion -> PlayMajorVersion.forPlayVersion(playVersion).getDefaultScalaPlatform()));
-        platform.getJavaVersion().set(JavaVersion.current());
-        injectedRoutesGenerator = objectFactory.property(Boolean.class);
-        injectedRoutesGenerator.set(false);
+        this.platform = objectFactory.newInstance(PlayPlatform.class, objectFactory);
+        this.platform.getPlayVersion().convention(DEFAULT_PLAY_VERSION);
+        this.platform.getJavaVersion().convention(JavaVersion.current());
+        this.platform.getScalaVersion().convention(platform.getPlayVersion().map(playVersion -> PlayMajorVersion.forPlayVersion(playVersion).getDefaultScalaPlatform()));
+        this.injectedRoutesGenerator = objectFactory.property(Boolean.class).convention(true);
     }
 
     /**
