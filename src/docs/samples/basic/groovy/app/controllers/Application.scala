64753a54480a8b6afc834f0265f7858214163c37
ryan <truongducthang@gmail.com>
2019-11-09 16:58:57 +0800

    Remove the typo in Application.scala

--- src/docs/samples/basic/groovy/app/controllers/Application.scala
+++ src/docs/samples/basic/groovy/app/controllers/Application.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 import org.apache.commons.lang.StringUtils
 
 @Singleton
-public class Application @Inject() extends InjectedController {
+class Application @Inject() extends InjectedController {
 
   def index = Action {
     Ok(views.html.index(StringUtils.trim("   Your new application is ready.   ")))
