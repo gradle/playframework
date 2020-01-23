5df503d400c4bd599d5c8a240f2084e4fe9a9146
Sterling Greene <sterling.greene@gmail.com>
2020-01-23 17:57:58 -0500

    Update versions of Play used in test coverage

--- src/integTestFixtures/groovy/org/gradle/playframework/fixtures/multiversion/PlayCoverage.groovy
+++ src/integTestFixtures/groovy/org/gradle/playframework/fixtures/multiversion/PlayCoverage.groovy
@@ -10,9 +10,11 @@ final class PlayCoverage {
 
     public static final VersionNumber DEFAULT = VersionNumber.parse(DEFAULT_PLAY_VERSION)
     public static final List<VersionNumber> ALL = [
+            VersionNumber.parse('2.3.10'),
             VersionNumber.parse('2.4.11'),
-            VersionNumber.parse('2.5.18'),
-            VersionNumber.parse('2.7.3'),
+            VersionNumber.parse('2.5.19'),
+            VersionNumber.parse('2.7.4'),
+            VersionNumber.parse('2.8.0'),
             DEFAULT
     ]
 }
--- src/main/java/org/gradle/playframework/extensions/PlayPlatform.java
+++ src/main/java/org/gradle/playframework/extensions/PlayPlatform.java
@@ -18,7 +18,7 @@ public class PlayPlatform {
     /**
      * The default Play version used if no Play version was configured.
      */
-    public static final String DEFAULT_PLAY_VERSION = "2.6.15";
+    public static final String DEFAULT_PLAY_VERSION = "2.6.25";
 
     private final Property<String> playVersion;
     private final Property<String> scalaVersion;
