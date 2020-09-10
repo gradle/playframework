924597f339e3421d028cb8c2bbf65ba175f48c0a
Cosmin Stroe <oncall@cosmin.cloud>
2020-09-09 23:55:20 -0500

    add detailed logging when running on TravisCI
    Signed-off-by: Cosmin Stroe <oncall@cosmin.cloud>

--- buildSrc/src/main/kotlin/org/gradle/playframework/TestSetupPlugin.kt
+++ buildSrc/src/main/kotlin/org/gradle/playframework/TestSetupPlugin.kt
@@ -3,6 +3,7 @@ package org.gradle.playframework
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.kotlin.dsl.withType
 
 
@@ -11,8 +12,13 @@ class TestSetupPlugin : Plugin<Project> {
         tasks.withType<Test>().configureEach {
             // Log test execution so that Travis CI doesn't time out
             if (System.getenv("CI") != null) {
+                // Enable detailed logging when running in Travis CI,
+                // so that we can understand why a build failed
+                // From: https://stackoverflow.com/a/47458666
                 testLogging {
-                    events("started")
+                    events("started", "passed", "skipped", "failed")
+                    showStackTraces = true
+                    exceptionFormat = TestExceptionFormat.FULL
                 }
             }
 
