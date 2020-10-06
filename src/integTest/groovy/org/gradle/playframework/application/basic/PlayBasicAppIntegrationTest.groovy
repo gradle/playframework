1fe42c1552f6d468639a9fc3ff46220bf9c0cedb
Sterling Greene <sterling.greene@gmail.com>
2020-10-06 02:06:10 -0400

    Add failing test for deprecation warnings

--- src/integTest/groovy/org/gradle/playframework/application/basic/PlayBasicAppIntegrationTest.groovy
+++ src/integTest/groovy/org/gradle/playframework/application/basic/PlayBasicAppIntegrationTest.groovy
@@ -3,11 +3,17 @@ package org.gradle.playframework.application.basic
 import org.gradle.playframework.PlayMultiVersionApplicationIntegrationTest
 import org.gradle.playframework.fixtures.app.BasicPlayApp
 import org.gradle.playframework.fixtures.app.PlayApp
+import org.gradle.testkit.runner.BuildResult
 
 class PlayBasicAppIntegrationTest extends PlayMultiVersionApplicationIntegrationTest {
 
     @Override
     PlayApp getPlayApp() {
         new BasicPlayApp(playVersion)
+    }
+
+    def "does not emit deprecation warnings"() {
+        expect:
+        build("build", "--warning-mode=fail")
     }
 }
