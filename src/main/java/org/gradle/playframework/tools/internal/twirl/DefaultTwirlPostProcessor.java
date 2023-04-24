909df3e7d54500d92f77abf5629a63e70a17219c
Jerome Prinet <jprinet@gradle.com>
2023-04-24 11:03:18 +0200

    CR review: Process lines by iteration instead of regex

--- src/main/java/org/gradle/playframework/tools/internal/twirl/DefaultTwirlPostProcessor.java
+++ src/main/java/org/gradle/playframework/tools/internal/twirl/DefaultTwirlPostProcessor.java
@@ -9,30 +9,25 @@ import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
+// This post processor fixes build / project dependent comments (DATE and SOURCE) from the TwirlCompiler generated files:
+//                    -- GENERATED --
+//                    DATE: Mon Apr 03 10:27:51 CEST 2023
+//                    SOURCE: /private/var/folders/79/xmc9yr493y75ptry2_nrx3r00000gn/T/junit4995996226044083355/app/views/test.scala.html
+//                    HASH: 4bbbe5fde39afa0d46da8df7714a136a78170d6a
+//                    MATRIX: 728->1|841->19|869->20|920->45|948->53
+//                    LINES: 21->1|26->1|26->1|26->1|26->1
+//                    -- GENERATED --
 class DefaultTwirlPostProcessor implements Serializable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultTwirlPostProcessor.class);
 
     private static final String GENERATED_TAG = "-- GENERATED --";
-    private static final String ANY_CHAR_INCLUDING_NEW_LINE = "[\\S\\n\\r\\s]";
-    private static final String ANY_LINES = ANY_CHAR_INCLUDING_NEW_LINE + "*";
-    private static final String ANY_LINE = ".*" + ANY_CHAR_INCLUDING_NEW_LINE;
-
-    // Example of text to match:
-    // /*
-    //                    -- GENERATED --
-    //                    DATE: Mon Apr 03 10:27:51 CEST 2023
-    //                    SOURCE: /private/var/folders/79/xmc9yr493y75ptry2_nrx3r00000gn/T/junit4995996226044083355/app/views/test.scala.html
-    //                    HASH: 4bbbe5fde39afa0d46da8df7714a136a78170d6a
-    //                    MATRIX: 728->1|841->19|869->20|920->45|948->53
-    //                    LINES: 21->1|26->1|26->1|26->1|26->1
-    //                    -- GENERATED --
-    //  */
-    private static final String PATTERN_MATCHER = "(/\\*" + ANY_LINES + GENERATED_TAG + ANY_LINES + ")(%s:\\s" + ANY_LINE + ")(" + ANY_LINES + GENERATED_TAG + ANY_LINES + "\\*/)";
+    private static final String GENERATED_LINE_PREFIX_DATE = "DATE: ";
+    private static final String GENERATED_LINE_PREFIX_SOURCE = "SOURCE: ";
 
     void execute(TwirlCompileSpec spec) {
         String sourceReplacementString = getSourceReplacementString(spec.getSources());
@@ -57,18 +52,24 @@ class DefaultTwirlPostProcessor implements Serializable {
 
     private void process(Path generatedFile, String sourceReplacementString) {
         try {
-            String content = new String(Files.readAllBytes(generatedFile), StandardCharsets.UTF_8);
-            String regexSource = String.format(PATTERN_MATCHER, "SOURCE");
-            Matcher matcherSource = Pattern.compile(regexSource, Pattern.MULTILINE).matcher(content);
-            if(matcherSource.find() && matcherSource.groupCount() == 3) {
-                content = matcherSource.replaceAll("$1" + sourceReplacementString + "\n$3");
+            List<String> generatedSourceLines = Files.readAllLines(generatedFile, StandardCharsets.UTF_8);
+            List<String> updatedSourceLines = new ArrayList<>();
+
+            boolean isInGeneratedSection = false;
+            for (String currentLine : generatedSourceLines) {
+                if(currentLine.contains(GENERATED_TAG)) {
+                    isInGeneratedSection = !isInGeneratedSection;
+                }
+                if(isInGeneratedSection && currentLine.contains(GENERATED_LINE_PREFIX_SOURCE)) {
+                    // update path to relative and keep trailing spaces
+                    String updatedLine = currentLine.substring(0, currentLine.indexOf(GENERATED_LINE_PREFIX_SOURCE)) + sourceReplacementString;
+                    updatedSourceLines.add(updatedLine);
+                } else if(!(isInGeneratedSection && currentLine.contains(GENERATED_LINE_PREFIX_DATE))) {
+                    updatedSourceLines.add(currentLine);
+                }
             }
-            String regexDate = String.format(PATTERN_MATCHER, "DATE");
-            Matcher matcherDate = Pattern.compile(regexDate, Pattern.MULTILINE).matcher(content);
-            if(matcherDate.find() && matcherDate.groupCount() == 3) {
-                content = matcherDate.replaceAll("$1\n$3");
-            }
-            Files.write(generatedFile, content.getBytes(StandardCharsets.UTF_8));
+
+            Files.write(generatedFile, updatedSourceLines, StandardCharsets.UTF_8);
         } catch (IOException e) {
             LOGGER.warn(String.format("Unable to post-process file %s", generatedFile.getFileName()), e);
         }
